7990e1dcea09c822f5e94f16ab4978b02d00aa07
abcde456 <gamerzee344@gmail.com>
2025-09-15 09:06:33 -0400

    First commit

--- opencvdetector.py
+++ opencvdetector.py
@@ -1,1 +1,22 @@
-# put apriltag opencv code here
+from pupil_apriltags import Detector
+from PIL import Image
+import numpy
+import cv2
+
+img = cv2.imread()
+img = img.convert("L")
+numpyImg = numpy.array(img, dtype=numpy.uint8)
+
+
+detector = Detector(
+                    families='tag36h11',
+                    nthreads=1,
+                    quad_decimate=1.0,
+                    quad_sigma=0.0,
+                    refine_edges=1,
+                    decode_sharpening=0.25,
+                    debug=0)
+
+tags = detector.detect(numpyImg, estimate_tag_pose=True, camera_params=[2.0, 3.7, 320.0, 240.0], tag_size=15.0)
+
+print(tags)
