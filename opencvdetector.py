08f34d6726eacd257000eedb8633b643bf448a6a
abcde456 <gamerzee344@gmail.com>
2025-09-25 09:14:48 -0400

    Added rotation and testing keybind

--- opencvdetector.py
+++ opencvdetector.py
@@ -6,7 +6,8 @@
 #  Description    : This program takes an input from a camera, then scans for
 # april tag position.
 #  Input          : Webacm.
-#  Output         : Print apriltag location on window of camera stream.
+#  Output         : Print apriltag location and orientation to
+#  window of camera stream.
 ###############################################################################
 
 from pyapriltags import Detector
@@ -15,9 +16,17 @@ import numpy
 import cv2
 from numpy import pi
 import euler
+import math
+import keyboard
 
 # Open camera
 cap = cv2.VideoCapture(0)
+
+tagSizeFloat = 0.15
+
+keyboard.add_hotkey('v', lambda: print('V was pressed'))
+keyboard.add_hotkey('m', lambda: print('M was pressed'))
+keyboard.add_hotkey('space', lambda: print('Space was pressed'))
 
 if not cap.isOpened():
     print("Error: Could not open webcam.")
@@ -56,29 +65,32 @@ while True:
     tags = detector.detect(
         numpyImg, estimate_tag_pose=True, 
         camera_params=[2.0, 3.7, 640.0, 480.0], 
-        tag_size=0.15
+        tag_size=tagSizeFloat
     )
 
     if(tags):
-        text = f"{tags[0].pose_t[0]*100}"
+        distText = f"Dist: {tags[0].pose_t[0]*100}"
         a, b, c = euler.angles("xyz", tags[0].pose_R)
-        textTwo = f"{a}, {b}, {c}"
+        rotText = f"Rot: X: {round(a*(180/math.pi), 2)}, "
+        rotText = rotText + f"Y: {round(b*(180/math.pi), 2)}, "
+        rotText = rotText + f"Z: {round(c*(180/math.pi), 2)}"
     else:
-        text="N/A"
-        textTwo = ""
+        distText="Dist: N/A"
+        rotText = "Rot: N/A"
     print(tags)
     with open('log.txt', 'a') as f:
-        f.write("\n"+text)
+        f.write("\n"+distText+"\n"+rotText)
 
     font = cv2.FONT_HERSHEY_SIMPLEX
     org = 00, 185
     orgTwo = 00, 145
+    orgThree = 50, 50
     fontScale = 0.5
     color = (0, 0, 255)
     thickness = 1
 
-    modifiedImg = cv2.putText(frame, 
-        text, 
+    distTextImg = cv2.putText(frame, 
+        distText, 
         org, 
         font, 
         fontScale, 
@@ -88,8 +100,8 @@ while True:
         False
     )
 
-    modifiedImgTwo = cv2.putText(modifiedImg, 
-        textTwo, 
+    rotTextImg = cv2.putText(distTextImg, 
+        rotText, 
         orgTwo, 
         font, 
         fontScale, 
@@ -99,7 +111,39 @@ while True:
         False
     ) 
 
-    cv2.imshow('Webcam Feed', modifiedImg)
+    promptText = ""
+
+    inPromptResize = False
+
+    if keyboard.is_pressed("space"):
+        inPromptResize = True
+        promptText = "Hold camera 30cm away from apriltag, then press m. To cancel, press v."
+        
+    
+    if keyboard.is_pressed("m"):
+        inPromptResize = False
+        promptText = ""
+    
+    if keyboard.is_pressed("v"):
+        if(inPromptResize):
+            tagSizeFloat = float(30/(tags[0].pose_t[0]*100))
+            promptText = "Resized"
+            inPromptResize = False
+
+    readyImg = cv2.putText(distTextImg, 
+        promptText, 
+        orgThree, 
+        font, 
+        fontScale, 
+        color, 
+        thickness, 
+        cv2.LINE_AA, 
+        False
+    ) 
+
+    cv2.imshow('Webcam Feed', readyImg)
+
+    promptText = ""
 
     # Press 'q' to exit
     if cv2.waitKey(1) & 0xFF == ord('q'):
